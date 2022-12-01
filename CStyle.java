455758492d112e83f96eef852298b4b87a5c0c14
cindycheng12 <103102173+cindycheng12@users.noreply.github.com>
2022-12-01 21:52:05 +0800

    Add files via upload

---
+++ CStyle.java
@@ -0,0 +1,11 @@
+package pack6.subpack1.subpack;
+
+//CStyle.java,此檔案置於pack6\subpack1\subsubpack內
+
+public class CStyle{
+  public int style;
+  public CStyle(int n){
+      style=n;
+      System.out.println("style"+style);
+  }
+}
